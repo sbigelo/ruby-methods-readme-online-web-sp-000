78dc29e4111962e5866262c8fd9f495071cb48f2
Sean Bigelow <sean.bigelow1@gmail.com>
2020-11-05 17:54:58 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -1,13 +1,9 @@
 # Use this space to code along with the readme
-
+def say_hello_ten_times
 phrase = "Hello World!"
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
+count = 0
+  when count < 10
+    puts phrase
+    count += 1
+  end
+end
